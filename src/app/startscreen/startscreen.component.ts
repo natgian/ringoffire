b6d729c0b260c15ad330a265a8fb39dedea940ea
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-10-15 22:30:41 +0200

    feat: add startscreen/game components and routing

---
+++ src/app/startscreen/startscreen.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-startscreen',
+  templateUrl: './startscreen.component.html',
+  styleUrls: ['./startscreen.component.scss']
+})
+export class StartscreenComponent {
+  constructor(private router: Router){}
+
+  newGame() {
+    // Start game
+    this.router.navigateByUrl("/game")
+  }
+}
