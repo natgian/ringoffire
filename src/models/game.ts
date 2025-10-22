f0ad2de6f1678c6c7d1a6506d85d9aff5dee4df3
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-10-22 21:28:16 +0200

    feat: add playerActive input to control active player styling

--- src/models/game.ts
+++ src/models/game.ts
@@ -2,7 +2,7 @@ export class Game {
   public players: string[] = [];
   public stack: string[] = [];
   public playedCards: string[] = [];
-  public currentPlayer: number = 0
+  public currentPlayer: number = 0;
 
   constructor() {
     for(let i = 1; i <= 13; i++) {
